276a26066266d6a18fb25d927e0adc77a2da8530
Zhong Jianxin <azuwis@gmail.com>
2020-07-15 23:32:05 +0800

    Build as library

---
+++ webpack.lib.js
@@ -0,0 +1,36 @@
+var merge = require('webpack-merge')
+var webpack = require('webpack')
+var path = require('path')
+var common = require('./webpack.common.js')
+var Cleaner = require('clean-webpack-plugin')
+
+var pathsToClean = [
+    'lib/**',
+    'dist/**',
+    'build/**'
+]
+
+module.exports = merge(common, {
+    entry: {
+        osme: './src/OSME/SourceGenerator/ExampleSourceGenerator.ts'
+    },
+    output: {
+        filename: '[name].min.js',
+        path: path.resolve(__dirname, 'lib'),
+        library: 'osme',
+        libraryTarget: 'umd'
+    },
+    externals: ['opensheetmusicdisplay', 'xmlbuilder'],
+    mode: 'production',
+    optimization: {
+        minimize: true
+    },
+    plugins: [
+        // build optimization plugins
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new Cleaner(pathsToClean, { verbose: true, dry: false })
+    ]
+})
